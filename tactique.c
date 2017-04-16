7057679d368d5b891946259cf27a3b414fae9586
Loic CHETY <loic.chety@gmail.com>
2017-04-16 14:54:12 +0200

    work

--- tactique.c
+++ tactique.c
@@ -6,7 +6,7 @@
 /*   By: lchety <lchety@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/11 12:34:47 by lchety            #+#    #+#             */
-/*   Updated: 2017/04/14 22:19:44 by lchety           ###   ########.fr       */
+/*   Updated: 2017/04/16 14:52:00 by lchety           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -145,9 +145,95 @@ void	diagonale(t_fil *dna)
 
 }
 
+// void	corner(t_fil *dna)
+// {
+// 	int tab[3] = {0, 0, 0};
+//
+// 	// if (dna->area[dna->map.w -2][2].sign == '.')
+// 		//wave(dna, dna->map.w - 1, 0, 100);
+// 	//
+// 	//if (dna->area[(dna->map.w / 2) + 5][dna->map.h - 1].sign == '.')
+//
+// 	//check_liberty(dna);
+//
+// 	dprintf(2, "KAMELOTT => %c\n", dna->area[0][dna->map.h].sign);
+// 	dprintf(2, "KAMELOTT => %d\n", dna->map.h);
+//
+// 	tab[0] = check_liberty(dna, (dna->map.w / 2) + 5, 0);
+// 	tab[1] = check_liberty(dna, (dna->map.w / 2) + 5, dna->map.h - 1);
+// 	tab[2] = check_liberty(dna, 10, dna->map.h -1);
+//
+// 	if (tab[0] && dna->area[(dna->map.w / 2) + 5][0].sign == '.')
+// 	{
+// 		dprintf(2, "UP OK ! ##############################################\n");
+// 		wave(dna, (dna->map.w / 2) + 5, 0, 120);
+// 	}
+// 	else if (tab[1] && dna->area[dna->map.w / 2][dna->map.h / 2].sign == '.')
+// 	{
+// 		dprintf(2, "MIDDLE OK ! ############################################\n");
+// 		wave(dna, dna->map.w / 2, dna->map.h / 2, 120);
+// 	}
+// 	else if (tab[1] && dna->area[(dna->map.w / 2) + 5][dna->map.h - 10].sign == '.')
+// 	{
+// 		dprintf(2, "LIGHT BOTTOM OK ! ##############################################\n");
+// 		wave(dna, (dna->map.w / 2) + 5, dna->map.h - 10, 120);
+// 	}
+// 	else if (tab[1] && dna->area[(dna->map.w / 2) + 5][dna->map.h - 1].sign == '.')
+// 	{
+// 		dprintf(2, "BOTTOM OK ! ##############################################\n");
+// 		wave(dna, (dna->map.w / 2) + 5, dna->map.h - 1, 120);
+// 	}
+// 	else if (tab[2] && dna->area[6][dna->map.h - 1].sign == '.')
+// 	{
+// 		dprintf(2, "LEFT MIDDLE OK ! ##############################################\n");
+// 		wave(dna, 6, dna->map.h - 1, 120);
+// 	}
+// 	else if (tab[2] && dna->area[0][dna->map.h / 2].sign == '.')
+// 	{
+// 		dprintf(2, "LEFT LIGHT BOTTOM OK ! ##############################################\n");
+// 		wave(dna, 0, dna->map.h / 2, 120);
+// 	}
+// 	else if (dna->area[(dna->map.w / 2)][dna->map.h / 2].sign == '.')
+// 	{
+// 		dprintf(2, "BOTTOM RIGHT OK ! ##############################################\n");
+// 		wave(dna, (dna->map.w / 2), dna->map.h / 2, 120);
+// 	}
+// }
+
+
+int		upper(t_fil *dna)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (i < dna->map.h)
+	{
+		while (j < dna->map.w)
+		{
+			if (dna->area[i][j].sign == dna->enemy_char)
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	find_bounds(t_fil *dna, int bounds[])
+{
+	//*bounds[0] = upper(dna);
+	dprintf(2, "pouet");
+}
+
+
 void	corner(t_fil *dna)
 {
 	int tab[3] = {0, 0, 0};
+	int bounds[6] = {0, 0, 0, 0, 0, 0};
+	//int test[];
 
 	// if (dna->area[dna->map.w -2][2].sign == '.')
 		//wave(dna, dna->map.w - 1, 0, 100);
@@ -163,45 +249,24 @@ void	corner(t_fil *dna)
 	tab[1] = check_liberty(dna, (dna->map.w / 2) + 5, dna->map.h - 1);
 	tab[2] = check_liberty(dna, 10, dna->map.h -1);
 
-	if (tab[0] && dna->area[(dna->map.w / 2) + 5][0].sign == '.')
+	if (tab[1] && dna->area[dna->map.w / 2 + 4][dna->map.h / 2 + 4].sign == '.')
 	{
-		dprintf(2, "UP OK ! ##############################################\n");
-		wave(dna, (dna->map.w / 2) + 5, 0, 120);
-	}
-	else if (tab[1] && dna->area[dna->map.w / 2][dna->map.h / 2].sign == '.')
-	{
-		dprintf(2, "MIDDLE OK ! ############################################\n");
-		wave(dna, dna->map.w / 2, dna->map.h / 2, 120);
-	}
-	else if (tab[1] && dna->area[(dna->map.w / 2) + 5][dna->map.h - 10].sign == '.')
-	{
-		dprintf(2, "LIGHT BOTTOM OK ! ##############################################\n");
-		wave(dna, (dna->map.w / 2) + 5, dna->map.h - 10, 120);
-	}
-	else if (tab[1] && dna->area[(dna->map.w / 2) + 5][dna->map.h - 1].sign == '.')
-	{
-		dprintf(2, "BOTTOM OK ! ##############################################\n");
-		wave(dna, (dna->map.w / 2) + 5, dna->map.h - 1, 120);
-	}
-	else if (tab[2] && dna->area[6][dna->map.h - 1].sign == '.')
-	{
-		dprintf(2, "LEFT MIDDLE OK ! ##############################################\n");
-		wave(dna, 6, dna->map.h - 1, 120);
-	}
-	else if (tab[2] && dna->area[0][dna->map.h / 2].sign == '.')
-	{
-		dprintf(2, "LEFT LIGHT BOTTOM OK ! ##############################################\n");
-		wave(dna, 0, dna->map.h / 2, 120);
-	}
-	else if (dna->area[(dna->map.w / 2)][dna->map.h / 2].sign == '.')
-	{
-		dprintf(2, "BOTTOM RIGHT OK ! ##############################################\n");
-		wave(dna, (dna->map.w / 2), dna->map.h / 2, 120);
+		dprintf(2, "CLOSE MIDDLE OK ! ############################################\n");
+		wave(dna, dna->map.w / 2 + 4, dna->map.h / 2 + 4, 120);
 	}
 
-	// if (dna->area[dna->map.w - 2][dna->map.h - 1].sign == '.' &&
-	// dna->area[dna->map.w - 1][dna->map.h - 3].sign == '.' )
-	// wave(dna, dna->map.w - 1, dna->map.h - 2, 200);
+	find_bounds(dna, bounds);
+
+
+	//if (lower())
+
+
+
+	// else if (tab[1] && dna->area[dna->map.w / 2 + 8][0].sign == '.')
+	// {
+	// 	dprintf(2, "CLOSE MIDDLE OK ! ############################################\n");
+	// 	wave(dna, dna->map.w / 2 + 8, 0, 120);
+	// }
 }
 
 /*
